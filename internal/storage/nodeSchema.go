ff2796e6d97a03fc0f89fa26ce179c4f77d94122
Ritu Raj <alchemyrecloak@gmail.com>
2025-08-23 14:49:40 +0530

    node schema as of now

---
+++ internal/storage/nodeSchema.go
@@ -0,0 +1,20 @@
+package storage
+
+type DataType string 
+
+const (
+	Delta  DataType = "delta"
+	Snapshot DataType = "snapshot"
+)
+
+type Node struct {
+	number int;  // the node number
+	version float32;  // the version this node corrsponds to 
+	lastSnapshotAncestor int; // ths last ancestor which was a snapshot node 
+
+	nodeType DataType; // snapshot or delta node
+	data interface{}; // holding the actual data which the node will store diffs or snapshot
+
+	parentNumber int;
+	childNodes []int;
+}
