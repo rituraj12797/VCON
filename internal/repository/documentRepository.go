e7d1c305678ed30fc4cdadbb975b5d79a16c8323
Ritu Raj <alchemyrecloak@gmail.com>
2025-09-05 00:04:23 +0530

    Respository for document added - this alows us to create an interface ( the document respository ) whihc will communicate with DataBase the work that ODM's do

---
+++ internal/repository/documentRepository.go
@@ -0,0 +1,154 @@
+package repository
+
+import (
+	"context"
+	"time"
+
+	"vcon/internal/schema"
+
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// This is the abstraction to define the interface through which we will tak with the DataBase since Go doesnot have a standard ODm we define it ourselvs the way we talk with DB.
+
+type DocumentRespository struct {
+	collection *mongo.Collection //
+}
+
+func NewDocumentRepository(db *mongo.Database) *DocumentRespository { // takes arguemtn of databae and return the interface to communicate with the DocumentCollection ( Document table ) inside that Db
+
+	return &DocumentRespository{
+		collection: db.Collection("document"), // return the collection as this "document" collection
+	}
+}
+
+func (r *DocumentRespository) CreateDocument(context context.Context, title string, hashes []string) (*schema.Document, error) {
+
+	// the context the database to communicate with the title string and th base file ( version 1)
+	// CRITICAL -> verify if a document with same title already exists or not ?? if exists dont create this file
+
+	var existingDoc schema.Document
+
+	err := r.collection.FindOne(context, bson.M{"title": title}).Decode(&existingDoc) // if doc found it will write into ecistingDoc
+
+	if err == nil { // if error is nil means a document is found
+		return nil, errors.New(" A document with same title already exists")
+	}
+
+	// say if file is not found is no found then what is the reason ??
+
+	// reason 1: the file was actually not found and ErrNoDocument was returned
+	// else somethign was wrong and error will be done
+	if err != mongo.ErrNoDocuments {
+		// means the erros not indicated to the same titled document being esisting error is eomthign else
+		return nil, err
+	}
+
+	// Proceed to create a new document
+	now := time.Now()
+
+	// crea a new document with a pseudo root node the 0th node
+
+	// the 0th node
+	pseudoNode := schema.Node{
+		ParrentNode:          -1,
+		LastSnapshotAncestor: -1,
+		NodeNumber:           0,
+		Depth:                0,
+		NodeType:             schema.NodeTypeSnapshot,
+
+		VersionString: "pseudoRoot",
+	}
+
+	// the 1st Node
+	rootNode := schema.Node{
+		ParrentNode:          0,
+		LastSnapshotAncestor: 1,
+		NodeNumber:           1,
+		Depth:                1,
+		NodeType:             schema.NodeTypeSnapshot,
+
+		VersionString: "base", // this is fixed the version string for first version is forced to be base
+		FileArray:     hashes,
+	}
+
+	doc := schema.Document{
+		Title:         title,
+		NumberOfNodes: 2, // pseudo Root and root
+		NodeArray:     []schema.Node{pseudoNode, rootNode},
+
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	result, err := r.collection.InsertOne(context, doc) // result = return the saved document from DB
+
+	if err != nil {
+		// panic(err)
+		return nil, err
+	}
+
+	doc.ID = result.InsertedID.(primitive.ObjectID)
+
+	return &doc, nil
+}
+
+// Simply adds the new node to the corrosponding Document document
+func (r *DocumentRespository) AddNode(ctx context.Context, docId primitive.ObjectID, newNode schema.Node) error {
+
+	// add this node to this document
+
+	// define the update query
+	// push newNode into the nodes
+	// increment numberOfNodes by 1
+	// set  updated_at at time.Now()
+
+	update := bson.M{
+		"$push": bson.M{"nodes": newNode},
+		"$inc":  bson.M{"numberOfNodes": 1},
+		"$set":  bson.M{"updated_at": time.Now()},
+	}
+
+	result, err := r.collection.UpdateByID(ctx, docId, update)
+
+	if err != nil {
+		return err
+	}
+
+	if result.ModifiedCount == 0 {
+		return errors.New("document not found or modified")
+	}
+
+	return nil
+
+}
+
+func (r *DocumentRespository) FindByID(ctx context.Context, docId primitive.ObjectID) (*schema.Document, error) {
+
+	var doc schema.Document
+	err := r.collection.FindOne(ctx, bson.M{"_id": docId}).Decode(&doc)
+
+	if err != nil {
+		// means the document was not found
+		return nil, err
+	}
+
+	return &doc, nil
+}
+
+func (r *DocumentRespository) FindByTitle(ctx context.Context, title string) (*schema.Document, error) {
+
+	var doc schema.Document
+	err := r.collection.FindOne(ctx, bson.M{"title": title}).Decode(&doc)
+
+	if err != nil {
+		// means the document was not found
+		return nil, err
+	}
+
+	return &doc, nil
+}
--- internal/schema/contentString.go
+++ internal/schema/contentString.go
@@ -1,18 +1,18 @@
 package schema
 
+type StringInteger struct {
+	ID      string `bson:"_id"` // identifier ( SHA 256 Hash ) will be mongoDB id as th would provide fater
+	Content string `bson:"content"`
+}
 
-type StringInteger struct{
-	ID int 	`bson:"_id"` // identifier will be mongoDB id as th would provide fater 
-	Content string	`bson:"content"`;
-}       
 /*
 
-EXAMPLE 
+EXAMPLE
 
 
-{ "_id": 45,  "content": "import React from 'react';" }
-{ "_id": 101, "content": "function App() {" }
-{ "_id": 800, "content": "  return <h1>Hello, World!</h1>;" }
-{ "_id": 2,   "content": "}" }
+{ "_id": "45",  "content": "import React from 'react';" }
+{ "_id": "101", "content": "function App() {" }
+{ "_id": "800", "content": "  return <h1>Hello, World!</h1>;" }
+{ "_id": "2",   "content": "}" }
 
 */
